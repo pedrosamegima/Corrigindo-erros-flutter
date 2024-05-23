25206c3ceca50e3d173125dcfc2bf9fb8ce3d182
pedrosamegima <phsamegima@gmail.com>
2024-05-23 11:25:47 -0300

    Corrigindo erros

---
+++ android/app/src/main/kotlin/com/example/corrigindo_erro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.corrigindo_erro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:corrigindo_erro/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
